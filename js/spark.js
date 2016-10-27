9f7eca912051a5761c35291502694141dacf2052
nyx <yyj4399@gmail.com>
2016-10-27 19:39:09 +0800

    ajax

--- js/spark.js
+++ js/spark.js
@@ -1,1 +1,10 @@
-console.log($.get('https://www.cloudring.cc/github/index/index'));
+window.onload=function(){
+    var this_url = window.location.host;
+    var ajax_url = '';
+    if(this_url == 'git_view.local.xyz'){
+        ajax_url='http://git_back.local.xyz/';
+    } else {
+        ajax_url='https://www.cloudring.cc/';
+    }
+    console.log($.get(ajax_url+'github/index/index'));
+}
