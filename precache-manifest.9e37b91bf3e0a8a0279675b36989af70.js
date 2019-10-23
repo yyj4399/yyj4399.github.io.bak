157ccf2cf68e8e694c3bb2cd0696d1ffed82e4b9
yyj4399 <admin@yyj4399.com>
2019-10-23 15:26:41 +0800

    new homepage

---
+++ precache-manifest.9e37b91bf3e0a8a0279675b36989af70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8cdeb6f47d8d2f6bd09f0969121c08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7182fd1960347534a7af",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bfa1d9e76adfe4c6c46a",
+    "url": "./static/css/main.62fa89f6.chunk.css"
+  },
+  {
+    "revision": "7182fd1960347534a7af",
+    "url": "./static/js/2.45f3d571.chunk.js"
+  },
+  {
+    "revision": "bfa1d9e76adfe4c6c46a",
+    "url": "./static/js/main.a9a32614.chunk.js"
+  },
+  {
+    "revision": "1afef0b308a7739f0f9a",
+    "url": "./static/js/runtime-main.fba76dbe.js"
+  },
+  {
+    "revision": "09f4569382c83e7cfac7f809b6be01f9",
+    "url": "./static/media/avatar.09f45693.jpeg"
+  }
+]);
--- src/js/blog/index.js
+++
@@ -1,7 +0,0 @@
-app.controller('myCtrl', function($scope, $connect) {
-  $scope.init = function() {
-    $connect.get('index/index', function (response) {
-      $scope.blog_list = response;
-    }, {ajax: 1});
-  }
-});
